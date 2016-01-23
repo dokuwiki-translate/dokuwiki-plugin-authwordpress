6711c5088791cf6712833874b43ca455d34c463f
Damien Regad <dregad@mantisbt.org>
2016-01-23 15:54:34 +0000

    Update "use Wordpress" message

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,7 +5,7 @@
  * @author Damien Regad <dregad@mantisbt.org>
  */
 
-$lang['user_list_use_wordpress'] = 'Please use Wordpress to manage users.';
+$lang['user_list_use_wordpress'] = 'This list is read-only. Please use Wordpress to manage users.';
 
 $lang['error_filters_unsupported'] = 'Filters are not supported.';
 $lang['error_connect_failed'] = 'Unable to connect to the WordPress database (%s)<br>Please check your settings.';
